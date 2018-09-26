22ef19068122917d87e3f4681177e1181e791cd8
Drovich <alexandre.rapetti@gmail.com>
2018-09-26 20:32:06 +0200

    connection établi et échange de 1 message possible

--- src/jalon01/client.c
+++ src/jalon01/client.c
@@ -7,32 +7,110 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
-void get_addr_info(const char* addr, const char* port, struct addrinfo** res){
-  //https://www.tutorialspoint.com/unix_sockets/socket_client_example.htm
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include <errno.h>
+
+
+
+int do_socket(int domain, int type, int protocol) {
+    int sockfd;
+    int yes = 1;
+//create the socket
+    sockfd = socket(domain, type, protocol);
+//check for socket validity
+
+// set socket option, to prevent "already in use" issue when rebooting the server right on
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        error("ERROR setting socket options");
+    return sockfd;
 }
 
-int main(int argc,char** argv) {
+
+void init_client_addr(int port, struct sockaddr_in *client_addr) {
+//clean the client_add structure
+    memset(client_addr,'\0', sizeof(*client_addr) );  //
+//cast the port from a string to an int
+//internet family protocol
+    client_addr->sin_family = AF_INET;
+//we bind to the IP of the server (@ de loopback)
+    inet_aton("127.0.0.1", &client_addr->sin_addr );
+//    fprintf(stdout,"serv_addr ? :%d erreur %d\n",&client_addr->sin_addr, errno);
+//we bind on the tcp port specified
+    client_addr->sin_port = htons(port);
+
+}
+
+
+void do_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    int res = connect(sockfd, addr, addrlen);
+    if (res != 0) {
+      printf("Probleme de connextion au server\n");
+    }
+
+}
+
+ssize_t do_read(int fd, void *buf, size_t count ){
+  return read( fd, buf, count);
+}
+
+ssize_t do_write(int fd, void *buf, size_t count){
+  return write(fd, buf, count);
+}
+
+int main(int argc,char** argv)
+{
 
     if (argc != 3)
     {
         fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
         return 1;
     }
-
-    struct addrinfo** res;
-    //get address info from the server
-    get_addr_info(s_addr, res)
-
+    char buffer[256];
+//get address info from the server
+//get_addr_info();
 
 //get the socket
-//s = do_socket()
+int s = do_socket(AF_INET, SOCK_STREAM, 0);
+fprintf(stdout,"socket crée ? :%d \n",errno);
+
+struct sockaddr_in c_addr;
+init_client_addr(atoi(argv[2]), &c_addr);
+fprintf(stdout,"init_client_addr ? :%d \n",errno);
 
 //connect to remote socket
-//do_connect()
+do_connect(s, (const struct sockaddr *)&c_addr, sizeof(c_addr));
+fprintf(stdout,"connection réussie ? :%d \n",errno);
 
 
 //get user input
-//readline()
+//const void *buf = readline();         // uselss
+
+  printf("Please enter the message: ");
+   bzero(buffer,256);
+   fgets(buffer,255,stdin);
+
+   int n = write(s, buffer, strlen(buffer));
+
+   if (n < 0) {
+     perror("ERROR writing to socket");
+     exit(1);
+  }
+//char *txt = readline(" Quel est votre message ? :");  // uselss
+//ssize_t reslt = send(s, buffer, sizeof(buffer), 0);   // useles
+
+  // Now read server response
+   bzero(buffer,256);
+   n = read(s, buffer, 255);
+
+   if (n < 0) {
+      perror("ERROR reading from socket");
+      exit(1);
+   }
+
+   printf("[SERVER] : %s\n",buffer);
+
 
 //send message to the server
 //handle_client_message()
--- src/jalon01/server.c
+++ src/jalon01/server.c
@@ -7,6 +7,8 @@
 #include <netinet/in.h>
 #include <assert.h>
 
+#include <errno.h>
+
 void error(const char *msg)
 {
     perror(msg);
@@ -81,6 +83,7 @@ int main(int argc, char** argv)
 
     //specify the socket to be a server socket and listen for at most 20 concurrent client
     listen(sock, 20);
+    fprintf(stdout,"En attente de connection ? :%d \n",errno);
 
     //buffer init
     char * in_buf = (char *) malloc(6*sizeof(char));
@@ -94,17 +97,34 @@ int main(int argc, char** argv)
       //accept connection from client
       c_addrlen = sizeof(*c_addr);
       c_sock = do_accept(sock, c_addr, &c_addrlen);
+      fprintf(stdout,"Connextion accepté ? :%d \n",errno);
 
       while(1){
         //read what the client has to say
-        do_read(c_sock, buffer);
+        //do_read(c_sock, buffer);
+        //recv(c_sock, buffer, sizeof(buffer), 0);
+        bzero(buffer,256);
+        int n = read( c_sock,buffer,255 );
 
-        //we write back to the client
-        do_write(c_sock, buffer);
+        if (n < 0) {
+          perror("ERROR reading from socket");
+          exit(1);
+        }
+        fprintf(stdout,"message reçu :%s  err: %d\n",buffer, errno);
+
+        ////we write back to the client
+        //do_write(c_sock, buffer); // anciennement
+        n = write(c_sock,buffer,255);
+        if (n < 0) {
+          perror("ERROR writing to socket");
+          exit(1);
+        }
 
         if(strcmp(buffer,"/quit")==0){
           break;
         }
+
+        break;
       }
 
       //clean up client socket
@@ -114,10 +134,11 @@ int main(int argc, char** argv)
 
       //check if server is stopped
       if(read(0, in_buf, 6) == -1)
-        error("Error reading from stdin");
+        error("Error reading from stdin\n");
       if(strcmp(in_buf,"/quit")==0){
         break;
       }
+      break; // temporaire
     }
 
     //clean up server socket
