05ac0ae8597eb1ccfb35cae870978958ddd20c38
STINSON Hugo <hstinson@maurice.pedago.ipb.fr>
2018-10-01 11:41:50 +0200

    Multiple clients can now connect to server

--- src/jalon01/server.c
+++ src/jalon01/server.c
@@ -6,9 +6,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <assert.h>
+#include <poll.h>
 
 #include <errno.h>
+
 #define BUFFER_SIZE 280
+#define MAX_CL 2
 
 void error(const char *msg)
 {
@@ -46,7 +49,17 @@ void do_read(int sock, char * buffer){
   bzero(buffer,BUFFER_SIZE);
   if(read(sock, buffer, BUFFER_SIZE) == -1)
     error("Error reading from client");
-  printf("> [Client] : %s",buffer);
+  printf("> [Client %d] : %s", sock, buffer);
+}
+
+int readline(int fd, char * buffer, int maxlen){
+  assert(buffer);
+  memset(buffer, 0, maxlen);
+  int size_read = read(fd, buffer, maxlen);
+  if(size_read==-1){
+    error("ERROR reading line");
+  }
+  return size_read;
 }
 
 void do_write(int sock, char * buffer){
@@ -61,9 +74,17 @@ void do_write(int sock, char * buffer){
   }
 }
 
+int get_smallest_available_fd(struct pollfd * fds){
+  int i;
+  for(i=2; i<MAX_CL+2; i++){
+    if(fds[i].fd==-1)
+      return i;
+  }
+  error("ERROR too many connected clients");
+}
+
 int main(int argc, char** argv)
 {
-
     if (argc != 2)
     {
         printf("usage: RE216_SERVER port\n");
@@ -81,7 +102,7 @@ int main(int argc, char** argv)
     do_bind(sock, s_addr);
 
     //specify the socket to be a server socket and listen for at most 20 concurrent client (but only one use Jalon01)
-    listen(sock, 20);
+    listen(sock, MAX_CL);
     printf("> Waiting for connection : \n");
 
     //buffer init
@@ -91,33 +112,71 @@ int main(int argc, char** argv)
     int c_addrlen;
     int c_sock;
 
+    struct pollfd fds[MAX_CL+2]; // = (struct pollfd *) malloc((MAX_CL+2)*sizeof(struct pollfd));
+    //fds = memset(fds, 0, (MAX_CL+2)*sizeof(struct pollfd));
+
+    fds[0].fd = 0;
+    fds[0].events = POLLIN;
+
+    fds[1].fd = sock;
+    fds[1].events = POLLIN;
+
+    int i;
+    for(i=2;i<MAX_CL+2;i++){
+      fds[i].fd = -1;
+      fds[i].events = POLLIN;
+    }
+
     for (;;)
     {
-      //accept connection from client
-      c_addrlen = sizeof(*c_addr);
-      c_sock = do_accept(sock, c_addr, &c_addrlen);
-      printf("> Connection accepted \n");
 
-      while(1){
-        printf("> Waiting for message\n");
-        //read what the client has to say
-        do_read(c_sock, buffer);
-        do_write(c_sock, buffer);
+      assert(fds);
+      if(poll(fds, MAX_CL+2, -1)==-1)
+        error("ERROR polling");
 
-        //disconect client if "/quit"
+      if(fds[0].revents & POLLIN){
+        memset(buffer, 0, BUFFER_SIZE);
+        readline(0, buffer, BUFFER_SIZE);
         if(strncmp(buffer,"/quit",5)==0){
-          printf("> Client disconnected\n");
+          printf("> Closing server\n");
           break;
+        }
+      }
+
+      if(fds[1].revents & POLLIN){
+        //accept connection from client
+        c_addrlen = sizeof(*c_addr);
+        c_sock = do_accept(sock, c_addr, &c_addrlen);
+        fds[get_smallest_available_fd(fds)].fd = c_sock;
+        printf("> Connection accepted \n");
+      }
+
+      for(i=2;i<MAX_CL+2; i++){
+        if(fds[i].revents & POLLIN){
+          c_sock = fds[i].fd;
+          //read what the client has to say
+          do_read(c_sock, buffer);
+          //server response
+          //disconnect client if "/quit in stdin"
+          if(strncmp(buffer,"/quit",5)==0){
+            printf("> Client disconnected\n");
+            fds[i].fd=-1; //we want to ignore this fd next loops
+            close(c_sock);
+            break;
+          }
+          else{
+            do_write(c_sock, buffer);
+          }
         }
       }
 
     //clean up client socket
     //free(buffer);
-    close(c_sock);
    }
 
     //clean up server socket
-    free(in_buf);
+    free(c_addr);
     close(sock);
     return 0;
+
 }
