03f3267d27727000069ba6c75ce558bd9b5be74d
Drovich <alexandre.rapetti@gmail.com>
2018-11-08 16:38:11 +0100

    Derniere fixation pour écrire les fichier dans un dossier spécifique, obtenir la taille du ficher à envoyer

--- src/jalon04/file_trans.h
+++ src/jalon04/file_trans.h
@@ -2,6 +2,7 @@
 #define _FILE_H_
 
 
+int size_of_file(char *path);
 
 int send_file( char *path, int fd_rcv_file);
 
@@ -9,4 +10,6 @@ int send_file( char *path, int fd_rcv_file);
 int fill_file(char * buffer, char *path, int size);
 
 
-int rcv_file(char *path, int fd_rcv_file);
+int rcv_file(char *path, int fd_rcv_file,int size);
+
+#endif
